d307ca65605e7bcac5319872c0200fa85f6225d0
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-08-25 15:29:57 -0300

    fix: env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 
 const AppError = require("./utils/AppError");
@@ -28,6 +29,6 @@ app.use((error , request, response, next) => {
   
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running in Port ${PORT}`));
